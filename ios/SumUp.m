cec0c226f8c47773d9d1e92e773b11e2c797935a
Andriy <andriy@mr-apps.com>
2020-03-18 10:37:23 +0100

    Added payment interface method

--- ios/SumUp.m
+++ ios/SumUp.m
@@ -1,38 +1,44 @@
 #import "SumUp.h"
+#import <React/RCTConvert.h>
 #import <SMPPayment/SMPPaymentRequest.h>
 
 @implementation SumUp
 
-RCT_EXPORT_METHOD(makePayment:(NSString *)key resolver:(RCTPromiseResolveBlock)resolve
-                  rejecter:(RCTPromiseRejectBlock)reject)
+- (NSString*)setPrefixIfNeeded: (NSString*)number
+{
+  if (number != nil && number.length == 10) {
+      return [@"+39" stringByAppendingString:number];
+  } else {
+      return number;
+  }
+}
+
+
+RCT_EXPORT_METHOD(makePayment:(NSDictionary *)request resolver:(RCTPromiseResolveBlock)resolve rejecter:(RCTPromiseRejectBlock)reject)
 {
 
-    SMPPaymentRequest *request;
-    NSDecimalNumber *amount = [NSDecimalNumber decimalNumberWithString:@"10.00"];
-
-    request = [SMPPaymentRequest paymentRequestWithAmount:amount
-                                                 currency:@"EUR"
-                                                    title:@"My title"
-                                             affiliateKey:@"e721bf67-7393-4511-a398-02e3c79acfc9"];
-
-    [request setCallbackURLSuccess:[NSURL URLWithString:@"phome://result"]];
-    [request setCallbackURLFailure:[NSURL URLWithString:@"phome://result"]];
+    NSDecimalNumber *total = [NSDecimalNumber decimalNumberWithString:[RCTConvert NSString:request[@"totalAmount"]]];
+    NSString *title = [RCTConvert NSString:request[@"title"]];
+    NSString *customerEmail = [RCTConvert NSString:request[@"customerEmail"]];
     
+    NSString *customerPhoneNumber = [RCTConvert NSString:request[@"customerPhoneNumber"]];
+    NSString *formattedPhoneNumber = [self setPrefixIfNeeded:(customerPhoneNumber)];
+    NSString *currencyCode = [RCTConvert NSString:request[@"currencyCode"]];
+    NSString *transactionId = [[NSUUID UUID] UUIDString];
+    
+    SMPPaymentRequest *paymentRequest = [SMPPaymentRequest
+                                            paymentRequestWithAmount:total
+                                            currency:currencyCode
+                                            title:title
+                                            affiliateKey:@"e721bf67-7393-4511-a398-02e3c79acfc9"];
 
-    /* This will open the following URL
-     * sumupmerchant://pay/1.0?amount=10.00&currency=EUR&affiliate-key=YOUR-AFFILIATE-KEY
-     * &title=My%20title
-     * &callbackfail=samplepaymentapp%3A%2F%2F
-     * &callbacksuccess=samplepaymentapp%3A%2F%2F
-     */
+    [paymentRequest setForeignTransactionID:transactionId];
+    [paymentRequest setReceiptPhoneNumber:formattedPhoneNumber];
+    [paymentRequest setReceiptEmailAddress:customerEmail];
+    [paymentRequest setCallbackURLSuccess:[NSURL URLWithString:@"phome://result"]];
+    [paymentRequest setCallbackURLFailure:[NSURL URLWithString:@"phome://result"]];
 
-    /* Optionally: Setting the option to skip success screens
-     * to add skip-screen-success=true to the URL
-     * Supported by SumUp app version 1.69 and later.
-     * [request setSkipScreenOptions:SMPSkipScreenOptionSuccess];
-     */
-
-    [request openSumUpMerchantApp];
+    [paymentRequest openSumUpMerchantApp];
 }
 
 RCT_EXPORT_MODULE();
