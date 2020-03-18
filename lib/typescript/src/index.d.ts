c9d2b8ddbd85dffaf63ccc2bab4222fb1c919f47
Andriy <andriy@mr-apps.com>
2020-03-18 11:51:09 +0100

    Added generated lib

---
+++ lib/typescript/src/index.d.ts
@@ -0,0 +1,16 @@
+export declare enum CurrencyCode {
+    EUR = "EUR",
+    USD = "USD"
+}
+declare type PaymentRequestData = {
+    title: string;
+    totalAmount: string;
+    customerEmail: string;
+    customerPhoneNumber: string;
+    currencyCode: CurrencyCode;
+};
+declare type SumUpType = {
+    makePayment(request: PaymentRequestData): Promise<void>;
+};
+declare const _default: SumUpType;
+export default _default;
